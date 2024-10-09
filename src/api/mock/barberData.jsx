26b235e96436960fd65620cbd25197dca8bf919b
thy <mailcuathy@gmail.com>
2024-10-09 11:10:52 +0700

    chore: rename barberInfo.js to barberData.js and update content

---
+++ src/api/mock/barberData.jsx
@@ -0,0 +1,133 @@
+export const about = {
+  title: "Giới thiệu về chúng tôi",
+  content: `Bạn đang cần tìm địa chỉ uy tín để đảm bảo an toàn cho cái đầu của bạn? đừng lo, có chúng tôi, đầu bạn tròn hay méo, béo hay gầy thì thầy cũng cân tất.
+        Với kinh nghiệm 100 năm cạo đầu, tôi đảm bảo đầu bạn bóng loáng hơn đèn pha ô tô
+        Chúng tôi đã tìm ra và đưa ra đư��ng d��n đầy đ�� cho bạn, giúp bạn d�� dàng tìm đâu đó.
+      `,
+  video_url:
+    "https://www.youtube.com/watch?v=sR5Z8AJ-zRU&ab_channel=MFHCoding%26Design",
+  //   youtube:
+  //   "https://www.youtube.com/watch?v=sR5Z8AJ-zRU&ab_channel=MFHCoding%26Design",
+  // facebook:
+  //   "https://www.facebook.com/watch/?ref=search&v=1419206862131554&external_log_id=1ca3bec0-afed-4e61-bdca-5b18b9155581&q=l%C3%BC%20m%C3%B4%20uy%E1%BB%83n",
+  // tiktok:
+  //   "https://www.tiktok.com/@juno_okyo/video/7417847068326202630?is_from_webapp=1&sender_device=pc",
+};
+
+export const banners = [
+  {
+    image_url:
+      "https://scontent.fhan14-1.fna.fbcdn.net/v/t39.30808-1/410567396_887020429722109_5688264758090841383_n.jpg?stp=cp6_dst-jpg_s200x200&_nc_cat=101&ccb=1-7&_nc_sid=0ecb9b&_nc_eui2=AeFhuiyNI2mahXRAy541wKSWQp9M8jPFL5xCn0zyM8UvnI7UsNEp9nYoeRbz46RMOrIhEVKeb4gt2P15nv_JBts8&_nc_ohc=vCZvYmCwsdQQ7kNvgH5LuQG&_nc_ht=scontent.fhan14-1.fna&_nc_gid=AlnMNPUCZ5JATLmQfKL56rD&oh=00_AYDOgwWhgrm3q-7_HPvDQDzoJAExq2bxeY4P2hXT0ABuzA&oe=670871B9",
+    name: "Ảnh 1",
+  },
+  { image_url: "/static/favicons/logo.png", name: "Ảnh 2" },
+  {
+    image_url:
+      "https://scontent.fhan14-1.fna.fbcdn.net/v/t39.30808-1/410567396_887020429722109_5688264758090841383_n.jpg?stp=cp6_dst-jpg_s200x200&_nc_cat=101&ccb=1-7&_nc_sid=0ecb9b&_nc_eui2=AeFhuiyNI2mahXRAy541wKSWQp9M8jPFL5xCn0zyM8UvnI7UsNEp9nYoeRbz46RMOrIhEVKeb4gt2P15nv_JBts8&_nc_ohc=vCZvYmCwsdQQ7kNvgH5LuQG&_nc_ht=scontent.fhan14-1.fna&_nc_gid=AlnMNPUCZ5JATLmQfKL56rD&oh=00_AYDOgwWhgrm3q-7_HPvDQDzoJAExq2bxeY4P2hXT0ABuzA&oe=670871B9",
+    name: "Ảnh 3",
+  },
+];
+
+export const productTypeList = {
+  product_type_title: "Sản phẩm",
+  product_type_data: [
+    { name: "Sản phẩm 1", image_url: "/static/favicons/logo.png" },
+    { name: "Sản phẩm 2", image_url: "/static/favicons/logo.png" },
+    { name: "Sản phẩm 3", image_url: "/static/favicons/logo.png" },
+    { name: "Sản phẩm 4", image_url: "/static/favicons/logo.png" },
+    { name: "Sản phẩm 5", image_url: "/static/favicons/logo.png" },
+    { name: "Sản phẩm 6", image_url: "/static/favicons/logo.png" },
+    { name: "Sản phẩm 7", image_url: "/static/favicons/logo.png" },
+    { name: "Sản phẩm 8", image_url: "/static/favicons/logo.png" },
+    { name: "Sản phẩm 9", image_url: "/static/favicons/logo.png" },
+  ],
+};
+
+export const serviceTypeList = {
+  service_type_title: "Dịch vụ",
+  service_type_data: [
+    { name: "Cắt tóc", image_url: "/static/favicons/logo.png" },
+    { name: "Gội đầu", image_url: "/static/favicons/logo.png" },
+    { name: "Tạo kiểu", image_url: "/static/favicons/logo.png" },
+  ],
+};
+
+export const barberList = {
+  barber_title: "Đội ngũ",
+  barber_data: [
+    {
+      name: "Trần Xuân Trường",
+      nickname: "Cây kéo bằng gỗ",
+      image_url:
+        "https://scontent.fhan14-1.fna.fbcdn.net/v/t39.30808-1/410567396_887020429722109_5688264758090841383_n.jpg?stp=cp6_dst-jpg_s200x200&_nc_cat=101&ccb=1-7&_nc_sid=0ecb9b&_nc_eui2=AeFhuiyNI2mahXRAy541wKSWQp9M8jPFL5xCn0zyM8UvnI7UsNEp9nYoeRbz46RMOrIhEVKeb4gt2P15nv_JBts8&_nc_ohc=vCZvYmCwsdQQ7kNvgH5LuQG&_nc_ht=scontent.fhan14-1.fna&_nc_gid=AlnMNPUCZ5JATLmQfKL56rD&oh=00_AYDOgwWhgrm3q-7_HPvDQDzoJAExq2bxeY4P2hXT0ABuzA&oe=670871B9",
+      description:
+        "Với hơn 15 năm kinh nghiệm trong nghề tỉa lông cho chó mèo, thì cái đầu của bạn cũng chỉ là muỗi",
+    },
+    {
+      name: "Đặng Hồng Sơn",
+      nickname: "Thủ lĩnh băng đảng",
+      image_url:
+        "https://scontent.fhan14-1.fna.fbcdn.net/v/t39.30808-1/410567396_887020429722109_5688264758090841383_n.jpg?stp=cp6_dst-jpg_s200x200&_nc_cat=101&ccb=1-7&_nc_sid=0ecb9b&_nc_eui2=AeFhuiyNI2mahXRAy541wKSWQp9M8jPFL5xCn0zyM8UvnI7UsNEp9nYoeRbz46RMOrIhEVKeb4gt2P15nv_JBts8&_nc_ohc=vCZvYmCwsdQQ7kNvgH5LuQG&_nc_ht=scontent.fhan14-1.fna&_nc_gid=AlnMNPUCZ5JATLmQfKL56rD&oh=00_AYDOgwWhgrm3q-7_HPvDQDzoJAExq2bxeY4P2hXT0ABuzA&oe=670871B9",
+      description: "Người với 2 bàn tay trắng dựng lên cơ đồ ",
+    },
+    {
+      name: "Lê Thế Thiết",
+      nickname: "Tông phát là đơ",
+      image_url:
+        "https://scontent.fhan14-1.fna.fbcdn.net/v/t39.30808-1/410567396_887020429722109_5688264758090841383_n.jpg?stp=cp6_dst-jpg_s200x200&_nc_cat=101&ccb=1-7&_nc_sid=0ecb9b&_nc_eui2=AeFhuiyNI2mahXRAy541wKSWQp9M8jPFL5xCn0zyM8UvnI7UsNEp9nYoeRbz46RMOrIhEVKeb4gt2P15nv_JBts8&_nc_ohc=vCZvYmCwsdQQ7kNvgH5LuQG&_nc_ht=scontent.fhan14-1.fna&_nc_gid=AlnMNPUCZ5JATLmQfKL56rD&oh=00_AYDOgwWhgrm3q-7_HPvDQDzoJAExq2bxeY4P2hXT0ABuzA&oe=670871B9",
+      description: "Người với 2 bàn tay trắng dựng lên cơ đồ ",
+    },
+  ],
+};
+
+export const achievementList = {
+  achievement_title: "Thành tựu đạt được",
+  achievement_data: [
+    {
+      name: "Top 1 Vietnam Top Fashion and Hair",
+      image_url: "/static/favicons/logo.png",
+    },
+    {
+      name: "Top 1 Color Zoom",
+      image_url: "/static/favicons/logo.png",
+    },
+  ],
+};
+
+export const customerList = {
+  customer_title: "Khách hàng của chúng tôi",
+  customer_data: [
+    {
+      name: "",
+      image_url: "/static/favicons/logo.png",
+    },
+    {
+      name: "",
+      image_url: "/static/favicons/logo.png",
+    },
+  ],
+};
+
+const barberInfo = {
+  barber_name: "tóc nam đẹp heyson",
+  barber_description: "gọt mọi loại đầu, cắt là đẹp, không đẹp vẫn lấy tiền",
+  barber_address: "Số 6, Ngõ 850 Đường Láng, Láng Thượng, Đống Đa, Hà Nội",
+  barber_phone: "0987654321",
+  social: [
+    { social_name: "Facebook", social_url: "https://facebook.com" },
+    { social_name: "Youtube", social_url: "https://youtube.com" },
+    { social_name: "Email", social_url: "mailto:example@example.com" },
+    { social_name: "TikTok", social_url: "https://tiktok.com" },
+    { social_name: "Instagram", social_url: "https://instagram.com" },
+    { social_name: "X", social_url: "https://x.com" },
+  ],
+  services: [
+    { service_name: "Cắt tóc tạo kiểu" },
+    { service_name: "Uốn tóc" },
+    { service_name: "Nhuộm tóc" },
+    { service_name: "Gội đầu" },
+  ],
+  products: [{ product_name: "Sáp vuốt tóc" }, { product_name: "Gôm xịt tóc" }],
+};
+
+export default barberInfo;

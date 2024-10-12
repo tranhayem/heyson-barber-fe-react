6e6d26c61e658f9429e217f88025ff450149a2e4
thy <mailcuathy@gmail.com>
2024-10-12 14:26:47 +0700

    feat: implement Products display using GenericSlider component

--- src/pages/customer/products/Products.jsx
+++ src/pages/customer/products/Products.jsx
@@ -1,7 +1,20 @@
-import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+
+import { barberShop } from "~/api/mock/barberData"; // Nhập dữ liệu từ barberData
+import GenericSlider from "~/components/custom/slider/GenericSlider";
 
 function Products() {
-  return <Box>Products</Box>;
+  return (
+    <Container>
+      {barberShop.map((shop, index) => (
+        <GenericSlider
+          key={index}
+          title={shop.product_type_name}
+          items={shop.product_data}
+        />
+      ))}
+    </Container>
+  );
 }
 
 export default Products;

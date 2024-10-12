3d52ac84a30e881e52d372095330ab4fd2846bc0
thy <mailcuathy@gmail.com>
2024-10-12 14:00:58 +0700

    feat: implement Services display using GenericSlider component

--- src/pages/customer/services/Services.jsx
+++ src/pages/customer/services/Services.jsx
@@ -1,7 +1,20 @@
-import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+
+import { barberService } from "~/api/mock/barberData";
+import GenericSlider from "~/components/custom/slider/GenericSlider";
 
 function Services() {
-  return <Box>Services</Box>;
+  return (
+    <Container>
+      {barberService.map((service, index) => (
+        <GenericSlider
+          key={index}
+          title={service.service_type_name}
+          items={service.service_data}
+        />
+      ))}
+    </Container>
+  );
 }
 
 export default Services;
